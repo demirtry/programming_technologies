9d8de75c54cd7bf8138479d0a110f3cd96791c23
Дмитрий <dimakol2005@mail.ru>
2024-11-07 21:11:06 +0500

    add doctest for simple program

--- SimpleProgram/Factorial.py
+++ SimpleProgram/Factorial.py
@@ -3,6 +3,16 @@ def factorial_recursive(n: int) -> int:
     Calculate factorial of a number using recursion.
     Arg: n (int): The number for which to calculate the factorial.
     Returns: (int) The factorial of the given number.
+    >>> factorial_recursive(-1)
+    -1
+    >>> factorial_recursive(0)
+    1
+    >>> factorial_recursive(1)
+    1
+    >>> factorial_recursive(5)
+    120
+    >>> factorial_recursive(10)
+    3628800
     """
     try:
         if n < 0:
@@ -13,3 +23,8 @@ def factorial_recursive(n: int) -> int:
             return n * factorial_recursive(n - 1)
     except ValueError:
         raise ValueError("Input must be >= 0.")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
